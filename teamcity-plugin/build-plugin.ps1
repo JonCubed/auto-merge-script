4ef3d5faff647c91a1c9fe32e7ae9fcc7f4d3614
Jonathan Kuleff <jonathankuleff@gmail.com>
2015-08-30 00:38:09 +1000

    added teamcity meta runner

---
+++ teamcity-plugin/build-plugin.ps1
@@ -0,0 +1,34 @@
+﻿$ErrorActionPreference = "Stop"
+
+cls
+
+$pluginFolder = ".\plugin"
+$packingFolder = ".\.artifacts"
+$agentFolder = Join-Path $packingFolder "agent"
+
+# Create .package folder
+if (-not (Test-Path $packingFolder))
+{
+    Write-Output "Creating $packingFolder"
+    New-Item $packingFolder -ItemType Directory
+}
+else
+{
+    Write-Output "Cleaning $packingFolder"
+    Remove-Item $packingFolder\* -Recurse -Force
+}
+
+Write-Output "Copying files to $packingFolder"
+Copy-Item $pluginFolder\* $packingFolder -Recurse
+Copy-Item ..\src\AutoMerge\AutoMerge.ps1,..\src\AutoMerge\AutoMerge.fsx $agentFolder\tools
+
+
+Write-Output "Creating plugin zip in $packingFolder"
+Compress-Archive -Path $agentFolder\* -DestinationPath $agentFolder\GitAutoMerge.zip
+Remove-Item $agentFolder\* -Exclude GitAutoMerge.zip -Recurse -Force
+
+Compress-Archive -Path $packingFolder\* -DestinationPath $packingFolder\GitAutoMerge-plugin.zip
+Remove-Item $packingFolder\* -Recurse -Exclude GitAutoMerge-plugin.zip -Force
+
+
+Write-Output "Build complete"
